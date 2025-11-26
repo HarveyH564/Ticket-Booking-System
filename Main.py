fd0abb9d379c1fecb1e639e689965035abb4a1ae
HarveyH564 <hh317@student.le.ac.uk>
2025-11-26 16:46:14 +0000

    within User.py changed userID to username, changed userPassword to password added createAccount() to Main.py added startMenu() to Main.py started login() in Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+import os
+import json
+
 def getEvents():
     # TO DO
     return
@@ -59,9 +62,71 @@ def deleteEvent():
     # TO DO
     return
 
+def handleInput(userInput):
+    if userInput == "<-":
+        return
+
+def createAccount():
+    invalidUsername = True
+    while invalidUsername:
+        username= input("Enter username: ")
+        # if user wants to go back
+        if username == "<-":
+            print()
+            return initialMenu()
+
+        if os.path.exists("users/" + username + ".json"):
+            print("Username is taken, please try again")
+        else:
+            userFile = open("users/" + username + ".json", "x")
+            password = input("Enter password: ")
+            # if user wants to go back
+            if password == "<-":
+                userFile.close()
+                os.remove("users/" + username + ".json")
+                print()
+                return initialMenu()
+            userInfo = {
+                "Username": username,
+                "Password": password,
+                "Tickets": {},
+                "Cart": {}
+            }
+            jsonInput = json.dumps(userInfo)
+            userFile.write(jsonInput)
+            userFile.close()
+            invalidUsername = False
+
+def login():
+    print("Enter username:")
+    username = input()
+    if not os.path.exists("users/" + username + ".json"):
+        print("User doesn't exist, please try again")
+
+
+def initialMenu():
+    # Welcome msg
+    print("Welcome to the project")
+    print("Enter [<-] to go back to the previous menu")
+
+    # Decides whether to navigate to create account or login
+    choicePending = True
+    while choicePending:
+        print("Select an option: Create an account [C], Login to an account [L]")
+        userInput = input()
+        if userInput == "C":
+            createAccount()
+            choicePending = True
+        elif userInput == "L":
+            login()
+            choicePending = True
+        elif userInput == "<-":
+            print("Cannot go back from initial menu\n")
+
+
 def Main():
     # TO DO
-    print("Welcome to the project")
-    return
+    initialMenu()
+
 
 start = Main()
