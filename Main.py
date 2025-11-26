d4f9b6313ef7df206604e74d2df3a6a50461ecce
HarveyH564 <hh317@student.le.ac.uk>
2025-11-26 17:17:25 +0000

    NOTE CREATE ACCOUNT AND LOGIN ARE VERY SIMPLE AND SHOULD BE REVISITED LATER within User.py changed userID to username, changed userPassword to password added createAccount() to Main.py added startMenu() to Main.py added login() in Main.py

--- Main.py
+++ Main.py
@@ -70,63 +70,102 @@ def createAccount():
     invalidUsername = True
     while invalidUsername:
         username= input("Enter username: ")
-        # if user wants to go back
+        # previous menu
         if username == "<-":
             print()
-            return initialMenu()
+            return False
 
         if os.path.exists("users/" + username + ".json"):
             print("Username is taken, please try again")
         else:
             userFile = open("users/" + username + ".json", "x")
             password = input("Enter password: ")
-            # if user wants to go back
+            # previous menu
             if password == "<-":
                 userFile.close()
                 os.remove("users/" + username + ".json")
                 print()
-                return initialMenu()
+                return False
             userInfo = {
-                "Username": username,
-                "Password": password,
-                "Tickets": {},
-                "Cart": {}
+                "username": username,
+                "password": password,
+                "tickets": {},
+                "cart": {}
             }
             jsonInput = json.dumps(userInfo)
             userFile.write(jsonInput)
             userFile.close()
             invalidUsername = False
+            print("Account creation successful")
+            return True
 
+# returns True if login was successful
 def login():
-    print("Enter username:")
-    username = input()
-    if not os.path.exists("users/" + username + ".json"):
-        print("User doesn't exist, please try again")
+    incorrectUsername = True
+    while incorrectUsername:
+        username = input("Enter username: ")
+        # previous menu
+        if username == "<-":
+            print()
+            return False
 
+        # check user name
+        if not os.path.exists("users/" + username + ".json"):
+            print("User doesn't exist, please try again")
+        else:
+            incorrectUsername = False
+            incorrectPassword = True
+            userFile = open("users/" + username + ".json", "r")
+            userInfo = json.loads(userFile.read())
+            userPassword = userInfo["password"]
 
+            # check password
+            while incorrectPassword:
+                passwordAttempt = input("Enter password: ")
+                # previous menu
+                if passwordAttempt == "<-":
+                    userFile.close()
+                    print()
+                    incorrectPassword = True
+                    return False
+                elif passwordAttempt == userPassword:
+                    userFile.close()
+                    incorrectPassword = True
+                    print("Login successful")
+                    return True
+                else:
+                    print("Incorrect password, please try again")
+
+# start menu for initialising program
+# returns true if login/create account was successful
 def initialMenu():
     # Welcome msg
     print("Welcome to the project")
     print("Enter [<-] to go back to the previous menu")
 
-    # Decides whether to navigate to create account or login
-    choicePending = True
-    while choicePending:
-        print("Select an option: Create an account [C], Login to an account [L]")
-        userInput = input()
+    # Loops the menu unless user logs in or creates account
+    while True:
+        userInput = input("Select an option: Create an account [C], Login to an account [L]: ")
         if userInput == "C":
-            createAccount()
-            choicePending = True
+            attempt = createAccount()
+            if attempt == True:
+                return True
         elif userInput == "L":
-            login()
-            choicePending = True
+            attempt = login()
+            if attempt == True:
+                return True
+        # previous menu
         elif userInput == "<-":
             print("Cannot go back from initial menu\n")
 
 
 def Main():
     # TO DO
-    initialMenu()
+
+    loggedIn = False
+    while not loggedIn:
+        # will become true if user logs in or creates account
+        loggedIn = initialMenu()
 
 
 start = Main()
